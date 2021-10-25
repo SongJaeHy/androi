8af922a6bd34168ca855d1bdc705f88b1e5b9687
SongJaeHy <rnswothd@naver.com>
2021-10-25 12:00:12 +0900

    12

--- TableCalculatorPrj/app/src/main/java/org/ict/tablecalculatorprj/MainActivity.java
+++ TableCalculatorPrj/app/src/main/java/org/ict/tablecalculatorprj/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     // 창부품 선언
@@ -20,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
             R.id.btn5,R.id.btn6,R.id.btn7,R.id.btn8,R.id.btn9,};
 
     // 이외 나머지 요소는 직접 선언
-    Button btnp,btnm,btnu,btnd;
+    Button btnp,btnm,btnu,btnd,btnc;
     EditText et1,et2;
     TextView tv2;
     String num1, num2;
@@ -42,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         btnm = (Button)findViewById(R.id.btnm);
         btnu = (Button)findViewById(R.id.btnu);
         btnd = (Button)findViewById(R.id.btnd);
+        btnc = (Button)findViewById(R.id.btnc);
 
         tv2 = (TextView) findViewById(R.id.tv2);
 
@@ -55,30 +57,59 @@ public class MainActivity extends AppCompatActivity {
                 public void onClick(View v) {
                     // 포커스된 EditText에 숫자 입력하기
                     if(et1.isFocused()){
-                        // et1에 커사가 깜빡이는(즉 선택된)경우
-                        num1 = et1.getText().toString()
-                                + numBtns[index].getText().toString();
-                        et1.setText(num1);
+                        // 0을 첫 글자로 입력하는걸 방지하는 로직 추가 예정
+
+                        if((et1.getText()+"").equals("0")){
+                            et1.setText(numBtns[index].getText());
+                        } else {
+                            et1.setText(et1.getText()+""+numBtns[index].getText());
+                            // et1에 커사가 깜빡이는(즉 선택된)경우
+                            //num1 = et1.getText().toString() + numBtns[index].getText().toString();
+                        }
+
                        // et1.setText(numBtns[index].getText());
+                        //et1.setText(et1.getText()+""+numBtns[index].getText());
 
                     } else if(et2.isFocused()){
-                        // et2에 커사가 깜빡이는(즉 선택된)경우
-                        num2 = et2.getText().toString()
-                                + numBtns[index].getText().toString();
+                        // 0을 첫 글자로 입력하는걸 방지하는 로직 추가 예정
+                        if((et2.getText()+"").equals("0")){
+                            et2.setText(numBtns[index].getText());
+                        } else {
+                            // et2에 커사가 깜빡이는(즉 선택된)경우
+                            et2.setText(et2.getText()+""+numBtns[index].getText());
+                        }
+                        //Toast.makeText(getApplicationContext(),"0으로 나누지 마세요", Toast.LENGTH_SHORT).show();
+
+                       // num2 = et2.getText().toString() + numBtns[index].getText().toString();
+
                         //et2.setText(numBtns[index].getText());
+                        //et2.setText(et2.getText()+""+numBtns[index].getText());
+
+                    } else {
+                        Toast.makeText(getApplicationContext(),
+                                "먼저 에디트텍스트를 선택하세요", Toast.LENGTH_SHORT).show();
+
                     }
+
                 }
             });
         }
         btnp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //int resultNum1 = Integer.parseInt(et1.getText() + "");
+                //int resultNum2 = Integer.parseInt(et2.getText() + "");
                 String lResult = et1.getText().toString();
                 String rResult = et2.getText().toString();
-                int result = Integer.parseInt(lResult) + Integer.parseInt(rResult);
+                if((et1.getText()+"").equals("") || (et2.getText()+"").equals("")){
+                //if (num1.equals("") || num2.equals("")) {
+                    Toast.makeText(getApplicationContext(), "입력 값이 비었습니다", Toast.LENGTH_SHORT)
+                            .show();
+                } else {
+                    int result = Integer.parseInt(lResult) + Integer.parseInt(rResult);
 
-                tv2.setText(result + "");
-
+                    tv2.setText(result + "");
+                }
             }
         });
         btnm.setOnClickListener(new View.OnClickListener() {
@@ -86,10 +117,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String lResult = et1.getText().toString();
                 String rResult = et2.getText().toString();
-                int result = Integer.parseInt(lResult) - Integer.parseInt(rResult);
+                if ((et1.getText()+"").equals("") || (et2.getText()+"").equals("")) {
+                    Toast.makeText(getApplicationContext(), "입력 값이 비었습니다", Toast.LENGTH_SHORT)
+                            .show();
+                } else {
+                    int result = Integer.parseInt(lResult) - Integer.parseInt(rResult);
 
-                tv2.setText(result + "");
-
+                    tv2.setText(result + "");
+                }
             }
         });
         btnu.setOnClickListener(new View.OnClickListener() {
@@ -97,10 +132,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String lResult = et1.getText().toString();
                 String rResult = et2.getText().toString();
-                int result = Integer.parseInt(lResult) * Integer.parseInt(rResult);
+                if ((et1.getText()+"").equals("") || (et2.getText()+"").equals("")) {
+                    Toast.makeText(getApplicationContext(), "입력 값이 비었습니다", Toast.LENGTH_SHORT)
+                            .show();
+                } else {
+                    int result = Integer.parseInt(lResult) * Integer.parseInt(rResult);
 
-                tv2.setText(result + "");
-
+                    tv2.setText(result + "");
+                }
             }
         });
         btnd.setOnClickListener(new View.OnClickListener() {
@@ -108,12 +147,31 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String lResult = et1.getText().toString();
                 String rResult = et2.getText().toString();
-                int result = Integer.parseInt(lResult) / Integer.parseInt(rResult);
 
-                tv2.setText(result + "");
+                if ((et1.getText()+"").equals("") || (et2.getText()+"").equals("")) {
+                    Toast.makeText(getApplicationContext(), "입력 값이 비었습니다", Toast.LENGTH_SHORT)
+                            .show();
+                } else {
+                    // num2가 0이면 나누지 않는다.
+                    if ((et2.getText()+"").equals("0")) {
+                        Toast.makeText(getApplicationContext(),
+                                "0으로 나누면 안됩니다!", Toast.LENGTH_SHORT).show();
 
+                    }
+
+                    int result = Integer.parseInt(lResult) / Integer.parseInt(rResult);
+
+                    tv2.setText(result + "");
+                }
+            }
+        });
+        btnc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                et1.setText("");
+                et2.setText("");
             }
         });
 
-    }
+    }// end onCreate
 }
