ddfc199a4492b4ce32c95b3b9d211de396fc317c
SongJaeHy <rnswothd@naver.com>
2021-11-10 18:15:48 +0900

    insert1

--- airapi/app/src/main/java/org/ict/airapi/retrofit/RetrofitInterface.java
+++ airapi/app/src/main/java/org/ict/airapi/retrofit/RetrofitInterface.java
@@ -1,5 +1,15 @@
 package org.ict.airapi.retrofit;
 
-public interface RetrofitInterface {
+import org.ict.airapi.VO.Example;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface RetrofitInterface {
+    @GET("http://openapi.seoul.go.kr:8088/527852424d71657436387564417a46/json")
+
+    Call<Example> getair(@Query("key") String key, @Query("Type") String Type,
+                         @Query("Service") String Service, @Query("Start_Index") Integer Start_Index,
+                         @Query("End_Index") Integer End);
 }
