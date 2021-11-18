62d17d41104c26bab998573485d38b15c25e41c3
SongJaeHy <rnswothd@naver.com>
2021-11-18 12:39:37 +0900

    11/18

--- airapi/app/src/main/java/org/ict/airapi/retrofit/RetrofitInterface.java
+++ airapi/app/src/main/java/org/ict/airapi/retrofit/RetrofitInterface.java
@@ -9,7 +9,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface RetrofitInterface {
-    @GET("http://openapi.seoul.go.kr:8088//json/ListAirQualityByDistrictService/1/10/")
+    @GET("http://openapi.seoul.go.kr:8088/6c465262567165743837634f504558/json/ListAirQualityByDistrictService/1/10/")
 
     Call<Example> getair(@Query("key") String key, @Query("Type") String Type,
                          @Query("Service") String Service, @Query("Start_Index") Integer Start_Index,
--- smallapi/app/src/main/java/org/ict/smallapi/retrofit/RetrofitInterface.java
+++ smallapi/app/src/main/java/org/ict/smallapi/retrofit/RetrofitInterface.java
@@ -7,7 +7,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface RetrofitInterface {
-    @GET("http://openapi.seoul.go.kr:8088//json/SeoulSmallLibraryInfo/1/5/")
+    @GET("http://openapi.seoul.go.kr:8088/6c465262567165743837634f504558/json/SeoulSmallLibraryInfo/1/5/")
 
     Call<Example> getsma(@Query("Key") String key, @Query("Type") String Type,
                          @Query("Service") String Service, @Query("Start_Index") Integer Start_Index,
